4f4728ffb38a64446f514aff7f393f43b62f7594
Anas AbuZaitoun <anasomarzaitoun@gmail.com>
2021-03-10 15:53:12 +0200

    Initial Commit

---
+++ CollectionViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewApp
+//
+//  Created by Anas AbuZaitoun on 10/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
